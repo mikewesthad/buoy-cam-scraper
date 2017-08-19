18c99ee6fe33d911dfaff4c62e9515502bd8ad24
Michael Hadley <michaelwesthadley@gmail.com>
2017-08-19 08:17:14 -0500

    try catch wrapper

--- src/scrape-cams.js
+++ src/scrape-cams.js
@@ -39,33 +39,37 @@ async function scrapeStations() {
 
     // Attempt to optimize for raspberry pi's limited resources... run this loop in sequence
     for (const stationId of stations) {
-        const utcSaved = Date.now();
-        const image = await jimp.read(`${endpoint}?station=${stationId}`);
-
-        // Check if the image is a white screen - indicates no recent buoy data. If so,
-        // skip it.
-        const whitePercent = getPercentWhite(image);
-        if (whitePercent > 0.95) {
-            console.log(`\t${stationId}: No buoy data. Skipping...`);
-            continue;
+        try {
+            const utcSaved = Date.now();
+            const image = await jimp.read(`${endpoint}?station=${stationId}`);
+    
+            // Check if the image is a white screen - indicates no recent buoy data. If so,
+            // skip it.
+            const whitePercent = getPercentWhite(image);
+            if (whitePercent > 0.95) {
+                console.log(`\t${stationId}: No buoy data. Skipping...`);
+                continue;
+            }
+    
+            // Check caption against last caption - this is more reliable than image diff check!
+            const captionImage = getBuoyCaptionImage(image);
+            const caption = await textRecognizer.recognizeText(captionImage.bitmap);
+            const utcFromBuoy = parseBuoyDate(caption.text.trim());
+            if (lastTimestamps[stationId] && lastTimestamps[stationId] === utcFromBuoy) {
+                console.log(`\t${stationId}: Caption matches last caption. Skipping...`);
+                continue;
+            }
+            
+            // All checks passed, save that image
+            console.log(`\t${stationId}: New image. Saving...`);
+            const imagePath = path.join(outputDirectory, `${utcFromBuoy}-${utcSaved}-${stationId}.jpg`);
+            image
+                .quality(75) // Match quality to what is returned from the server
+                .write(imagePath);
+            lastTimestamps[stationId] = utcFromBuoy;
+        } catch (err) {
+            console.error(err);
         }
-
-        // Check caption against last caption - this is more reliable than image diff check!
-        const captionImage = getBuoyCaptionImage(image);
-        const caption = await textRecognizer.recognizeText(captionImage.bitmap);
-        const utcFromBuoy = parseBuoyDate(caption.text.trim());
-        if (lastTimestamps[stationId] && lastTimestamps[stationId] === utcFromBuoy) {
-            console.log(`\t${stationId}: Caption matches last caption. Skipping...`);
-            continue;
-        }
-        
-        // All checks passed, save that image
-        console.log(`\t${stationId}: New image. Saving...`);
-        const imagePath = path.join(outputDirectory, `${utcFromBuoy}-${utcSaved}-${stationId}.jpg`);
-        image
-            .quality(75) // Match quality to what is returned from the server
-            .write(imagePath);
-        lastTimestamps[stationId] = utcFromBuoy;
     }
 
     console.log(`Scraping completed in: ${(Date.now() - startTime) / 1000 / 60} min.\n\n`);
