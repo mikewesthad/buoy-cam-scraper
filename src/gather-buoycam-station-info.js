06b7fd894037803775868eb2acf04645a61b4da2
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-29 19:58:59 -0600

    Update to axios over request-promise

--- src/gather-buoycam-station-info.js
+++ src/gather-buoycam-station-info.js
@@ -1,20 +1,24 @@
-const rp = require("request-promise");
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios");
 
 const outAllInfoPath = path.resolve(__dirname, "../data/buoycam-info.json");
 const outIDPath = path.resolve(__dirname, "../data/buoycam-ids.json");
 
 async function main() {
   try {
-    const result = await rp("http://www.ndbc.noaa.gov/buoycams.php");
-    const json = JSON.parse(result);
+    const response = await axios.get("http://www.ndbc.noaa.gov/buoycams.php");
+    const { data, status } = response;
+
+    if (status !== 200) {
+      throw new Error("Could not get buoycam data from endpoint.");
+    }
 
     // Dump the whole station listing, in case it's useful.
-    fs.writeFileSync(outAllInfoPath, JSON.stringify(json, null, 2));
+    fs.writeFileSync(outAllInfoPath, JSON.stringify(data, null, 2));
 
     // Filter out just the IDs.
-    const ids = json.map(stationInfo => parseInt(stationInfo.id, 10));
+    const ids = data.map(stationInfo => parseInt(stationInfo.id, 10));
     fs.writeFileSync(outIDPath, JSON.stringify(ids, null, 2));
   } catch (e) {
     console.error(e);
